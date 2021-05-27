90b18075eec987c68ca640e4114f54b66036c9e7
manibpl94 <manishpatel@appointy.com>
2021-05-27 17:27:37 +0530

    folder updated

---
+++ landing-pages/enterprise-appointment-scheduling-software/plugins/appointy_wp_bakery/js/custom.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function() {
+    jQuery('#vertical').lightSlider({
+      gallery:true,
+      item:1,
+      vertical:true,
+      vThumbWidth:270,
+      thumbItem:8,
+      thumbMargin:0,
+      slideMargin:0,
+      auto:true,
+      controls: false,
+      loop:true,
+      pauseOnHover: true,
+    });  
+  });
