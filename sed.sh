635fab2b101610abbe2a3360c6b18ff78e41fc61
manibpl94 <manishpatel@appointy.com>
2021-05-28 23:23:44 +0530

    updated

--- sed.sh
+++ sed.sh
@@ -1,1 +1,5 @@
 find . -type f -name index.html -print0 | xargs -0 sed -i -e "s|/cache/asset-cleanup/css/head-.*\.css|/xy.css|g"
+find . -type f -name index.html -print0 | xargs -0 sed -i  -e "s/http:/https:/g"
+find . -type f -name index.html -print0 | xargs -0 sed -i  -e "s|/appointy.com|/qa-www.appointy.com|g"
+find . -type f -name index.html -print0 | xargs -0 sed -i  -e "s|/wp-content|/$Module|g"
+find . -type f -name index.html -print0 | xargs -0 sed -i  -e "s|/wp-includes|/$Module|g"
